6002ab7a8226dec049137d00fcab384454228f95
Paulboca <paulboca1999@gmail.com>
2020-03-09 09:33:59 +0200

    Add files via upload

---
+++ Pacient.java
@@ -0,0 +1,36 @@
+// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 09.03.2020 09:03:15
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   Pacient.java
+
+import java.util.Vector;
+
+public class Pacient extends Persoana
+{
+
+    public Pacient()
+    {
+    }
+
+    public String getFisaPacient()
+    {
+        return null;
+    }
+
+    public void setFisaPacient()
+    {
+    }
+
+    public int getId()
+    {
+        return 0;
+    }
+
+    public void setId(int i)
+    {
+    }
+
+    private String fisaPacientului;
+    private int id;
+    public Vector myMedic;
+}
---
+++ Persoana.java
@@ -0,0 +1,54 @@
+// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 09.03.2020 09:03:46
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   Persoana.java
+
+
+public class Persoana
+{
+
+    public Persoana()
+    {
+    }
+
+    public int login(String userName, String password)
+    {
+        return 0;
+    }
+
+    public int register(String userName, String password, String email)
+    {
+        return 0;
+    }
+
+    public void setNume(String s)
+    {
+    }
+
+    public String getNume()
+    {
+        return null;
+    }
+
+    public void setPrenume(String s)
+    {
+    }
+
+    public String getPrenume()
+    {
+        return null;
+    }
+
+    public void setEmail(String s)
+    {
+    }
+
+    public String getEmail()
+    {
+        return null;
+    }
+
+    private String nume;
+    private String prenume;
+    private String email;
+}
